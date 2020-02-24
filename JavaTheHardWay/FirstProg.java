2f6a0aa460a9d05d6e71567aeff343b3d5bff250
stevie-steve <sharpsrjr@gmail.com>
2020-02-24 15:30:15 -0700

    Finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,7 @@
+public class FirstProg {
+    public static void main( String[] args) {
+      System.out.println( "You are determined to learn how to code, Steve." );
+      System.out.println( "Today's date is unknown" );
+
+    }
+}
